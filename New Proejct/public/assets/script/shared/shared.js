e8961e2a0fd0fae475e1a8ebd15c0ac915258de6
patilvijay9400 <patilvijay416940@gmail.com>
2024-02-07 00:53:56 +0530

    home changes

--- New Proejct/public/assets/script/shared/shared.js
+++ New Proejct/public/assets/script/shared/shared.js
@@ -13,6 +13,7 @@ const swiper = new Swiper('.swiper', {
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true
     },
   
     // Navigation arrows
@@ -29,32 +30,29 @@ const swiper = new Swiper('.swiper', {
 
   var swiperBrand = new Swiper("#swiper-brand", {
     direction: 'horizontal',
-    slidesPerView: 1.5,
+    slidesPerView: 1,
     spaceBetween: 30,
-    loop:false,
-    // autoplay: {
-    //     delay: 3000,
-    //   },
+    loop:true,
+    autoplay: {
+        delay: 3000,
+      },
     pagination: {
       el: "#pagination-02",
+      clickable: true
     },
     breakpoints: {
       0: {
         slidesPerView: 1,
       },
       520: {
-        slidesPerView: 1.5,
+        slidesPerView: 2,
       },
       768: {
-        slidesPerView: 2.5,
-      },
-      1000: {
         slidesPerView: 3,
       },
-
-      1200:{
-        slidesPerView:3
-      }
+      1400: {
+        slidesPerView: 4,
+      },
     },
   });
 
@@ -62,44 +60,29 @@ const swiper = new Swiper('.swiper', {
 
   var swiperCategory = new Swiper("#swiper-category", {
     direction: 'horizontal',
-    spaceBetween: 1,
-    // loop:true,
+    spaceBetween: 30,
+    loop:true,
     // autoplay: {
     //     delay: 1000,
     //   },
 
     pagination: {
       el: "#pagination-03",
+      clickable: true
     },
     breakpoints: {
       0: {
         slidesPerView: 1,
-        
-      },
-      360: {
-        slidesPerView: 1,
       },
       520: {
-        slidesPerView: 1.8,
-      },
-      768: {
         slidesPerView: 2,
       },
-      1000: {
-        slidesPerView: 2.5,
+      768: {
+        slidesPerView: 3,
       },
-
-      1200:{
-        slidesPerView:3
+      1400: {
+        slidesPerView: 4,
       },
-
-      1400:{
-        slidesPerView:3.5
-      },
-
-      1600:{
-        slidesPerView:4.5
-      }
     },
     
   });
